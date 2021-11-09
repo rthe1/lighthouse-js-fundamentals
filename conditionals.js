0aeda21f3e60718d26037a39048cc1039884fd29
Reshawn <reshawnhamilton97@gmail.com>
2021-11-09 14:08:46 +0000

    conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,5 @@
+//Weather Example
+
 const temperature = 12;
 // if the temperature = 16 or more console.log("Short sleeves are fine.");
 // if the temperature = -1 or less console.log("Make sure you pick out a scarf!");
@@ -11,3 +13,18 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+//Citizen Example
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+//Freeze OR Boil Example
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
